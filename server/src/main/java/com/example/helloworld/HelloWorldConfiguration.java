980e0d8e0658ced108cba7d09b8f64b5aaf503b4
Mehdi Houshmand <med1985@gmail.com>
2015-06-28 14:08:36 +0100

    Fixed up the server, ready for code

--- server/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ server/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,8 +1,6 @@
 package com.example.helloworld;
 
-import com.example.helloworld.core.Template;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -46,10 +44,6 @@ public class HelloWorldConfiguration extends Configuration {
         this.defaultName = defaultName;
     }
 
-    public Template buildTemplate() {
-        return new Template(template, defaultName);
-    }
-
     @JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
         return database;
@@ -58,19 +52,5 @@ public class HelloWorldConfiguration extends Configuration {
     @JsonProperty("database")
     public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
         this.database = dataSourceFactory;
-    }
-
-    @JsonProperty("viewRendererConfiguration")
-    public Map<String, Map<String, String>> getViewRendererConfiguration() {
-        return viewRendererConfiguration;
-    }
-
-    @JsonProperty("viewRendererConfiguration")
-    public void setViewRendererConfiguration(Map<String, Map<String, String>> viewRendererConfiguration) {
-        ImmutableMap.Builder<String, Map<String, String>> builder = ImmutableMap.builder();
-        for (Map.Entry<String, Map<String, String>> entry : viewRendererConfiguration.entrySet()) {
-            builder.put(entry.getKey(), ImmutableMap.copyOf(entry.getValue()));
-        }
-        this.viewRendererConfiguration = builder.build();
     }
 }
